742cc2671929a03e468abd0b7ce0d1f61b21f63f
Kaelin Alexander <kaelin.alexander@gmail.com>
2020-07-03 17:28:55 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     time = Time.new
-    if time.hour < 12
+    if time.hour <= 11
       resp.write "Good Morning!"
     elsif time.hour >= 12
       resp.write "Good Afternoon!"
